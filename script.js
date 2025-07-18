f4aa5ebe61a858134fda4f3e0c55c6c09d183fb4
sulistianaa <suliez.tiana16@gmail.com>
2025-07-18 15:06:40 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value;
+
+  // Simulasi user login
+  const users = {
+    "admin@pertamina.com": { password: "admin123", role: "admin" },
+    "hc@pertamina.com": { password: "hc123", role: "hc" },
+    "atasan@[ertamina].com": { password: "atasan123", role: "atasan" }
+  };
+
+  if (users[email] && users[email].password === password) {
+    const role = users[email].role;
+
+    if (role === "admin") {
+      window.location.href = "admin/dashboard.html";
+    } else if (role === "hc") {
+      window.location.href = "hc/dashboard.html";
+    } else if (role === "atasan") {
+      window.location.href = "atasan/dashboard.html";
+    }
+  } else {
+    document.getElementById("errorMsg").style.display = "block";
+  }
+});
